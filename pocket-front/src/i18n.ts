bce171ac565fcadece99362f3f59f2351d96447c
alexandre.rey <alexandre.rey74@outlook.com>
2024-10-15 18:23:56 +0200

    add autodetect language

--- pocket-front/src/i18n.ts
+++ pocket-front/src/i18n.ts
@@ -2,6 +2,7 @@ import { initReactI18next } from "react-i18next";
 import { en } from "./translations/en";
 import { fr } from "./translations/fr";
 import i18n from "i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 const resources = {
     ...en,
@@ -10,9 +11,11 @@ const resources = {
 
 i18n
     .use(initReactI18next)
+    .use(LanguageDetector)
     .init({
         resources,
-        lng: "en",
+        supportedLngs: ['en', 'fr'],
+        fallbackLng: "en",
         interpolation: {
             escapeValue: false
         }
